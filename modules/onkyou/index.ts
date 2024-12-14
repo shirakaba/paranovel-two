38cbf3ad1e32e2bf200bb94159b5a13f4bcdb2a7
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-12-14 18:15:21 +0900

    migrate Onkyou from expo-modules-core v1 to v2
    Based on: https://github.com/expo/expo/pull/28946/files#diff-15c704681d08785c321494455c6823db3df41693cc15c46f176910b9c04d077c

--- modules/onkyou/index.ts
+++ modules/onkyou/index.ts
@@ -1,8 +1,4 @@
-import {
-  NativeModulesProxy,
-  EventEmitter,
-  Subscription,
-} from 'expo-modules-core';
+import { EventSubscription } from 'expo-modules-core';
 
 // Import the native module. On web, it will be resolved to Onkyou.web.ts
 // and on native platforms to Onkyou.ts
@@ -17,12 +13,12 @@ export async function setValueAsync(value: string) {
   return await OnkyouModule.setValueAsync(value);
 }
 
-const emitter = new EventEmitter(OnkyouModule ?? NativeModulesProxy.Onkyou);
-
+// Migrated from expo-modules-core v1 to v2 based on:
+// https://github.com/expo/expo/pull/28946/files#diff-15c704681d08785c321494455c6823db3df41693cc15c46f176910b9c04d077c
 export function addChangeListener(
-  listener: (event: TimeupdateEventPayload) => void
-): Subscription {
-  return emitter.addListener<TimeupdateEventPayload>('onTimeupdate', listener);
+  listener: (event: TimeupdateEventPayload) => void,
+): EventSubscription {
+  return OnkyouModule.addListener('onTimeupdate', listener);
 }
 
 export { OnkyouView, OnkyouViewProps, TimeupdateEventPayload };
